2d3271e105dfd5719ffd3dee5fc759e1fe595c24
Yonathan <berithyjr@gmail.com>
2020-02-13 21:05:41 -0600

    changes

--- frontEnd/abogapi/src/components/layout/layout.js
+++ frontEnd/abogapi/src/components/layout/layout.js
@@ -1,11 +1,28 @@
 import React from "react"
 import NavBar from "./navbar"
 import Footer from "./footer"
+import CardLawyer from "./cards"
 
 
 
 const Layout = (props) =>{
-  return (<div><NavBar/></div>)
+  return (
+    <div>
+      <div><NavBar/></div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div><CardLawyer/></div>
+
+
+
+    </div>)
+
+
 
 }
 export default Layout
